117b4c62d6de02516e8816b2e1b592b0935354d3
Szabo Tamas <tamas.szabo0102@gmail.com>
2022-04-08 13:06:47 +0200

    b change

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,8 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        firingSuccess = true;
+        //this is the new edit
+	firingSuccess = true;
 
         break;
     }
